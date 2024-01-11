2cf90d9f5a35446db9893e08156998d1786f8d4b
Michael Shyu <michaelshyu11@gmail.com>
2024-01-11 15:42:57 -0500

    finished the lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  let returnStatement;
+
+  if (value <= 400) {
+    returnStatement = 'This one is on me!';
+  } else if (value <= 2000) {
+    returnStatement = 'That will be twenty bucks.';
+  } else if (value < 2500) {
+    returnStatement = 'I will gladly take your thirty bucks.';
+  } else {
+    returnStatement = 'No can do.';
+  }
+
+  return returnStatement;
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip) {
+    case 'generous' : 
+      return 'Thank you so much.';
+    case 'not as generous' :
+      return 'Thank you.';
+    default :
+      return 'Bye.';
+  }
+
 }
